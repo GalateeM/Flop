81ac86f61fc659b1e3d72c07270151033291db3e
PRG <prenaud@irit.fr>
2022-10-26 01:00:42 +0200

    (feat)gql query

---
+++ FlOpEDT/api_graphql/dummy/query.py
@@ -0,0 +1,20 @@
+from graphene import List
+from graphene_django.filter import DjangoFilterConnectionField
+
+from api_graphql.base import BaseQuery
+from . import resolvers as resolve
+
+from .types import DepartmentNode, TrainingProgrammeQL
+
+class Query(BaseQuery):
+    departments = DjangoFilterConnectionField(
+        DepartmentNode,
+        description="A list of departments.",
+        resolver=resolve.all_departments
+    )
+    training_programmes = List(
+        lambda: TrainingProgrammeQL,
+        description="A list of training programmes.",
+        resolver=resolve.all_training_programmes
+    )
+    
