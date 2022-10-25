2eb9fb5db48fdcf60aee52456a306e64d43e9aba
PRG <prenaud@irit.fr>
2022-10-26 00:45:11 +0200

    (feat)gql un truc malin pour diviser le code

---
+++ FlOpEDT/api_graphql/base.py
@@ -0,0 +1,12 @@
+from graphene.types.objecttype import ObjectType, ObjectTypeOptions
+
+class BaseQuery(ObjectType):
+    pass
+
+
+class BaseMutation(ObjectType):
+    pass
+
+
+class BaseSubscription(ObjectType):
+    pass
