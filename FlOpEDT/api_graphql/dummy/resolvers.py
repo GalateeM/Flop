940b4a52465d5b44f4142a92c581d1f6fcd72eb9
vivien7 <stephensonvivien1@gmail.com>
2022-10-27 23:11:27 +0200

    Suppression du rep dummy et de test_dummy

--- FlOpEDT/api_graphql/dummy/resolvers.py
+++
@@ -1,7 +0,0 @@
-from base.models import Department, TrainingProgramme
-
-def all_departments(root, info):
-    return Department.objects.all()
-
-def all_training_programmes(root, info):
-    return TrainingProgramme.objects.all()
