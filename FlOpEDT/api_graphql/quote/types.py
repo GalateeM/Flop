1f167744f56f1661d46617f466aecb0f5cfd4655
PRG <prenaud@irit.fr>
2022-11-09 11:05:35 +0100

    class Meta

--- FlOpEDT/api_graphql/quote/types.py
+++ FlOpEDT/api_graphql/quote/types.py
@@ -6,21 +6,22 @@ from . import resolvers as resolve
 from quote.models import Quote
 
 class QuoteNode(DjangoObjectType):
-    model = Quote
-    filter_fields = {
-        'quote' : ['icontains', 'istartswith'],
-        'last_name' : ['icontains', 'istartswith'],
-        'for_name' : ['icontains', 'istartswith'],
-        'nick_name' : ['icontains', 'istartswith'],
-        'desc_author' : ['icontains', 'istartswith'],
-        'date' : ['icontains', 'istartswith'],
-        'header' : ['icontains', 'istartswith'],
-        'quote_type__name' :['icontains', 'istartswith'],
-        'quote_type__abbrev' :['icontains', 'istartswith'],
-        'positive_votes' : ['exact'],
-        'negative_votes' : ['exact'],
-        'id_acc' : ['exact'],
-        'statut' : ['icontains', 'istartswith']
-    }
-    fields = '__all__'
-    interfaces = (relay.Node, )
+    class Meta:
+        model = Quote
+        filter_fields = {
+            'quote' : ['icontains', 'istartswith'],
+            'last_name' : ['icontains', 'istartswith'],
+            'for_name' : ['icontains', 'istartswith'],
+            'nick_name' : ['icontains', 'istartswith'],
+            'desc_author' : ['icontains', 'istartswith'],
+            'date' : ['icontains', 'istartswith'],
+            'header' : ['icontains', 'istartswith'],
+            'quote_type__name' :['icontains', 'istartswith'],
+            'quote_type__abbrev' :['icontains', 'istartswith'],
+            'positive_votes' : ['exact'],
+            'negative_votes' : ['exact'],
+            'id_acc' : ['exact'],
+            'statut' : ['icontains', 'istartswith']
+        }
+        fields = '__all__'
+        interfaces = (relay.Node, )
