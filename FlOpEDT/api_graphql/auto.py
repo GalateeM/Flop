a6d2b7bf4b177c3dc9703d28186c9cb748dae25a
Pablo <pablo.seban@univ-tlse2.fr>
2023-01-05 14:58:39 +0100

    add comments in auto.py file

--- FlOpEDT/api_graphql/auto.py
+++ FlOpEDT/api_graphql/auto.py
@@ -8,7 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 def schema_operations_builder(operationName, operationModule, operationBase, clsName):
-
+    """
+    Cette fonction fabrique un type unique, qui porte le nom operationName, qui contient toutes les classes
+    qui s'appellent avec un nom contenant clsName (sauf la classe operationBase) et ayant comme propriétés toutes les
+    propriétés de ces classes
+    """
     op_base_classes = build_base_classes(operationName, operationModule, operationBase, clsName)
 
     if len(op_base_classes) <= 1:
@@ -31,6 +35,7 @@ def build_base_classes(operationName, operationModule, operationBase, clsName):
 
     current_directory = os.path.dirname(os.path.abspath(__file__))
     current_module = current_directory.split('/')[-1]
+    # subdirectories contient tous les répertoires de api_graphql (ou presque)
     subdirectories = [
         x for x in os.listdir(current_directory)
         if os.path.isdir(os.path.join(current_directory, x))
@@ -43,12 +48,15 @@ def build_base_classes(operationName, operationModule, operationBase, clsName):
 
     for directory in subdirectories:
         try:
+            # j'essaie d'importer le fichier api_graphql.nom_du_rep.operationModule
             module = importlib.import_module(
                 '{0}.{1}.{2}'.format("api_graphql", directory, operationModule)
             )
             if module:
                 classes = [x for x in getmembers(module, isclass)]
                 opers = [x[1] for x in classes if clsName in x[0] and x[0] != operationBase]
+                # opers contient toutes les classes du fichier query.py (s'il existe) à condition qu'elles aient un nom
+                # qui contient clsname Et que leur nom ne soit pas operationBase
                 op_base_classes += opers
             else:
                 logger.info('wat?')
