e9f4e5b9fe62f96b78432c8e4985c68a5510282d
DELMAS Théo <theo_delmas@orange.fr>
2023-02-17 13:21:53 +0000

    feature: Refactoring ConstraintManager

---
+++ FlOpEDT/frontend/src/components/DisplayDocInNewConstraint.vue
@@ -0,0 +1,79 @@
+<template>
+    <TriggeredTeleporter
+            to=".modal-content"
+            :disable="DESACTIVATE_TELEPORTS"
+            :listeningTarget="listeningTarget"
+            eventName="contextmenu">
+        <p>Soon here the documentation's place</p>
+    </TriggeredTeleporter>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TriggeredTeleporter from '@/components/TriggeredTeleporter.vue'
+
+
+
+const DESACTIVATE_TELEPORTS = ref(false)
+
+/*
+================================ ADAPTATER ================================ 
+*/
+const listeningTarget = document.getElementById('nav-new-constraint') as EventTarget
+
+</script>
+
+<style scoped>
+.buttonContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: larger;
+}
+
+.plusButton {
+    width: 100%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    background-color: green;
+}
+
+.plusButton:hover {
+    background-color: darkgreen;
+}
+
+.minusButton {
+    width: 100%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    background-color: firebrick;
+}
+
+.minusButton:hover {
+    background-color: darkred;
+}
+
+.scrollbar {
+    max-height: 70vh;
+    overflow-y: scroll;
+}
+
+.scrollbar-primary::-webkit-scrollbar {
+    width: 12px;
+}
+
+.scrollbar-primary::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: dodgerblue;
+}
+.scrollbar-primary::-webkit-scrollbar-thumb:hover {
+    border-radius: 4px;
+    background-color: royalblue;
+}
+
+.scrollbar-primary {
+    scrollbar-color: #aaaaaa #f5f5f5;
+}
+</style>
---
+++ FlOpEDT/frontend/src/components/DisplayDocInPopover.vue
@@ -0,0 +1,130 @@
+<template>
+    <TriggeredTeleporter to=".popover-body" :disable="DESACTIVATE_TELEPORTS" :listeningTarget="listeningTarget"
+        eventName="contextmenu">
+        <hr />
+        <div>
+            <div class="buttonContainer">
+                <button id="doc-show-btn" :class="showBtnClassDefiner()" @click="swap()">
+                    {{ showDoc ? '⬆' : '⬇' }}
+                </button>
+            </div>
+            <template v-if="showDoc">
+                <div class="scrollbar scrollbar-primary">
+                    <DocumentationControler :constraint="selectedConstraint" />
+                </div>
+            </template>
+        </div>
+</TriggeredTeleporter>
+</template>
+
+<script setup lang="ts">
+import TriggeredTeleporter from  '@/components/TriggeredTeleporter.vue'
+import DocumentationControler from  '@/components/DocumentationControler.vue'
+
+import type { Constraint } from '@/models/Constraint';
+import { ref } from 'vue'
+
+
+const DESACTIVATE_TELEPORTS = ref(false)
+
+interface Props {
+    listeningTarget: EventTarget,
+    selectedConstraint: Constraint
+}
+const props = withDefaults(defineProps<Props>(), {})
+
+
+/**
+ * Enlarge the width of the parent popover & center the bottons Duplicate/Modify/Delete
+ */
+ function enlargePopover() {
+    const popover = document.getElementsByClassName('popover').item(0) as HTMLElement
+    if (popover !== null) {
+        popover.style['max-width'] = '80vw'
+    }
+    const groupeOfButton = document.getElementsByClassName('btn-group').item(0) as HTMLElement
+    if (groupeOfButton !== null) {
+        groupeOfButton.style['align-items'] = 'center'
+        groupeOfButton.style['justify-content'] = 'center'
+        groupeOfButton.style['display'] = 'flex'
+    }
+}
+
+
+/**
+ * Reference to know if the documentation is shown
+ */
+ const showDoc = ref(false)
+
+/**
+ * Swap showDoc value
+ */
+function swap() {
+    showDoc.value = !showDoc.value
+}
+
+/**
+ * doc-show-btn class definer
+ * permit to setup the display parameters
+ */
+const showBtnClassDefiner = () => {
+    enlargePopover()
+    return showDoc.value ? ' minusButton ' : ' plusButton '
+}
+
+</script>
+
+<style scoped>
+.buttonContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: larger;
+}
+
+.plusButton {
+    width: 100%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    background-color: green;
+}
+
+.plusButton:hover {
+    background-color: darkgreen;
+}
+
+.minusButton {
+    width: 100%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    background-color: firebrick;
+}
+
+.minusButton:hover {
+    background-color: darkred;
+}
+
+.scrollbar {
+    max-height: 70vh;
+    overflow-y: scroll;
+}
+
+.scrollbar-primary::-webkit-scrollbar {
+    width: 12px;
+}
+
+.scrollbar-primary::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: dodgerblue;
+}
+.scrollbar-primary::-webkit-scrollbar-thumb:hover {
+    border-radius: 4px;
+    background-color: royalblue;
+}
+
+.scrollbar-primary {
+    scrollbar-color: #aaaaaa #f5f5f5;
+}
+</style>
