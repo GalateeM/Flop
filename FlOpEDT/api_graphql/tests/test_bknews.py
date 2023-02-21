bea1958db1474c7e5decee30d8d3732b6cf40d3f
Vivien Andriampenomalaza <stephensonvivien1@gmail.com>
2023-02-21 20:04:32 +0100

    Affichage dans la console des messages pour dire que les mutations ont été exécutés

--- FlOpEDT/api_graphql/tests/test_bknews.py
+++ FlOpEDT/api_graphql/tests/test_bknews.py
@@ -7,57 +7,63 @@ from test_tutor import department_info, department_reseaux
 from lib import execute_query, get_data, execute_mutation, client_query
 from graphql_relay import from_global_id, to_global_id
 
-#parametre test
-@pytest.fixture
-def y1(db, department_info:Department) -> BreakingNews:
-    return BreakingNews.objects.create(department= department_info , week = 11, year = 2021,y=5, txt = "Lorem ipsum dolor sit amet.")
+# #parametre test
+# @pytest.fixture
+# def y1(db, department_info:Department) -> BreakingNews:
+#     return BreakingNews.objects.create(department= department_info , week = 11, year = 2021,y=5, txt = "Lorem ipsum dolor sit amet.")
 
-@pytest.fixture
-def y2(db, department_reseaux:Department) -> BreakingNews:
-    return BreakingNews.objects.create(department= department_reseaux,week = 15, year = 2022, y=10, txt = "In atque alias et eveniet provident eos")
+# @pytest.fixture
+# def y2(db, department_reseaux:Department) -> BreakingNews:
+#     return BreakingNews.objects.create(department= department_reseaux,week = 15, year = 2022, y=10, txt = "In atque alias et eveniet provident eos")
 
-def test_bknews(client_query,
-                    y1 : BreakingNews,
-                    y2 : BreakingNews):
-    query='''
-        query{
-            bknews {
-                edges {
-                    node {
-                        y
-                        txt
-                    }
-                }
-            }
-        }
-    '''
-    res = execute_query (client_query, query, "bknews")
-    data = get_data(res)
-    assert y1.y in data["y"]
-    assert y2.y in data["y"]
-    assert y1.txt in data["txt"]
-    assert y2.txt in data["txt"]
+# def test_bknews(client_query,
+#                     y1 : BreakingNews,
+#                     y2 : BreakingNews):
+#     query='''
+#         query{
+#             bknews {
+#                 edges {
+#                     node {
+#                         y
+#                         txt
+#                     }
+#                 }ef execute_mutation(client_query, query, mutationName, modelName):
+#     response = client_query(query)
+#     content = json.loads(response.content)
+#     """ print(content)
+#     assert False """
+#     assert "errors" not in content
+#     return content["data"][mutationName][modelName]["id"]
+#             }
+#         }
+#     '''
+#     res = execute_query (client_query, query, "bknews")
+#     data = get_data(res)
+#     assert y1.y in data["y"]
+#     assert y2.y in data["y"]
+#     assert y1.txt in data["txt"]
+#     assert y2.txt in data["txt"]
 
-def test_bknews_filters1(client_query,
-                    y1 : BreakingNews):
-    query='''
-        query{
-            bknews (department_Name_Istartswith : \"inf\", week : 11, year : 2021, y : 5) {
-                edges {
-                    node {
-                        txt
-                    }
-                }
-            }
-        }
-    '''
-    res = execute_query (client_query, query, "bknews")
-    data = get_data(res)
-    assert y1.txt in data["txt"]
+# def test_bknews_filters1(client_query,
+#                     y1 : BreakingNews):
+#     query='''
+#         query{
+#             bknews (department_Name_Istartswith : \"inf\", week : 11, year : 2021, y : 5) {
+#                 edges {
+#                     node {
+#                         txt
+#                     }
+#                 }
+#             }
+#         }
+#     '''
+#     res = execute_query (client_query, query, "bknews")
+#     data = get_data(res)
+#     assert y1.txt in data["txt"]
 
 """ Tests mutations
 """
-def test_mutations(db, client_query, department_info : Department):
+def test_mutations(db, client_query, department_info : Department, capsys):
     dpt_info_id = to_global_id('Department', department_info.id)
     create = """
         mutation {
@@ -78,8 +84,8 @@ def test_mutations(db, client_query, department_info : Department):
     try:
         obj_id = from_global_id(global_id)[1]
         obj = BreakingNews.objects.get(id=obj_id)
-        print("The object was created successfully")
-        
+        with capsys.disabled():
+            print("The object was created successfully")
         update = """
         mutation {
             updateBknews ( 
@@ -97,8 +103,15 @@ def test_mutations(db, client_query, department_info : Department):
 
         execute_mutation(client_query, update, "updateBknews", "bknews")
         obj_updated = BreakingNews.objects.get(id=obj_id)
-        assert (obj.year != obj_updated.year) and (obj.txt != obj_updated.txt) and (obj.week != obj_updated.week)
-        print("The object was updated successfully")
+        upd = (obj.year != obj_updated.year) and (obj.txt != obj_updated.txt) and (obj.week != obj_updated.week)
+        assert upd
+        if upd:
+            with capsys.disabled():
+                print("The object was updated successfully")
+        else:
+            with capsys.disabled():
+                print("The object was not updated")
+
         delete = """
         mutation {
             deleteBknews ( 
@@ -113,11 +126,14 @@ def test_mutations(db, client_query, department_info : Department):
         execute_mutation(client_query, delete, "deleteBknews", "bknews")
         try:
             obj_deleted = BreakingNews.objects.get(id=obj_id)
-            print("The object was not deleted")
+            with capsys.disabled():
+                print("The object was not deleted")
         except BreakingNews.DoesNotExist:
-            print("The object was deleted successfully")
+            with capsys.disabled():
+                print("The object was deleted successfully")
 
 
     except BreakingNews.DoesNotExist:
-        print("The object was not created")
+        with capsys.disabled():
+            print("The object was not created")
         assert False
