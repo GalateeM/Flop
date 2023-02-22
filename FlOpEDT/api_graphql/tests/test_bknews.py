f9504422c36413b4424041d3b184f6ece4667f24
Vivien Andriampenomalaza <stephensonvivien1@gmail.com>
2023-02-22 16:15:38 +0100

    Test bknews mutations

--- FlOpEDT/api_graphql/tests/test_bknews.py
+++ FlOpEDT/api_graphql/tests/test_bknews.py
@@ -8,58 +8,54 @@ from lib import execute_query, get_data, execute_mutation, client_query
 from graphql_relay import from_global_id, to_global_id
 
 # #parametre test
-# @pytest.fixture
-# def y1(db, department_info:Department) -> BreakingNews:
-#     return BreakingNews.objects.create(department= department_info , week = 11, year = 2021,y=5, txt = "Lorem ipsum dolor sit amet.")
+@pytest.fixture
+def y1(db, department_info:Department) -> BreakingNews:
+    return BreakingNews.objects.create(department= department_info , week = 11, year = 2021,y=5, txt = "Lorem ipsum dolor sit amet.")
 
-# @pytest.fixture
-# def y2(db, department_reseaux:Department) -> BreakingNews:
-#     return BreakingNews.objects.create(department= department_reseaux,week = 15, year = 2022, y=10, txt = "In atque alias et eveniet provident eos")
+@pytest.fixture
+def y2(db, department_reseaux:Department) -> BreakingNews:
+    return BreakingNews.objects.create(department= department_reseaux,week = 15, year = 2022, y=10, txt = "In atque alias et eveniet provident eos")
 
-# def test_bknews(client_query,
-#                     y1 : BreakingNews,
-#                     y2 : BreakingNews):
-#     query='''
-#         query{
-#             bknews {
-#                 edges {
-#                     node {
-#                         y
-#                         txt
-#                     }
-#                 }ef execute_mutation(client_query, query, mutationName, modelName):
-#     response = client_query(query)
-#     content = json.loads(response.content)
-#     """ print(content)
-#     assert False """
-#     assert "errors" not in content
-#     return content["data"][mutationName][modelName]["id"]
-#             }
-#         }
-#     '''
-#     res = execute_query (client_query, query, "bknews")
-#     data = get_data(res)
-#     assert y1.y in data["y"]
-#     assert y2.y in data["y"]
-#     assert y1.txt in data["txt"]
-#     assert y2.txt in data["txt"]
+""" Tests query
+"""
+def test_bknews(client_query,
+                    y1 : BreakingNews,
+                    y2 : BreakingNews):
+    query='''
+        query{
+            bknews {
+                edges {
+                    node {
+                        y
+                        txt
+                    }
+                }
+            }
+        }
+    '''
+    res = execute_query (client_query, query, "bknews")
+    data = get_data(res)
+    assert y1.y in data["y"]
+    assert y2.y in data["y"]
+    assert y1.txt in data["txt"]
+    assert y2.txt in data["txt"]
 
-# def test_bknews_filters1(client_query,
-#                     y1 : BreakingNews):
-#     query='''
-#         query{
-#             bknews (department_Name_Istartswith : \"inf\", week : 11, year : 2021, y : 5) {
-#                 edges {
-#                     node {
-#                         txt
-#                     }
-#                 }
-#             }
-#         }
-#     '''
-#     res = execute_query (client_query, query, "bknews")
-#     data = get_data(res)
-#     assert y1.txt in data["txt"]
+def test_bknews_filters1(client_query,
+                    y1 : BreakingNews):
+    query='''
+        query{
+            bknews (department_Name_Istartswith : \"inf\", week : 11, year : 2021, y : 5) {
+                edges {
+                    node {
+                        txt
+                    }
+                }
+            }
+        }
+    '''
+    res = execute_query (client_query, query, "bknews")
+    data = get_data(res)
+    assert y1.txt in data["txt"]
 
 """ Tests mutations
 """
--- FlOpEDT/api_graphql/tests/test_tutor.py
+++ FlOpEDT/api_graphql/tests/test_tutor.py
@@ -7,7 +7,8 @@ from test_scheduled_course import week1, week7, module_algo_prog, module_concept
 department_miashs, period_1 , period_2, tutor_algo_prog, tutor_conception, \
 course_type_algo, room_algo, course_algo, scheduled1, \
 course_type_conception, room_conception, course_conception, scheduled2
-from lib import *
+from lib import execute_query, get_data, execute_mutation, client_query
+from graphql_relay import from_global_id, to_global_id
 
 @pytest.fixture
 def department_info(db) -> Department:
@@ -33,41 +34,123 @@ def tutor_reseaux(db, department_reseaux : Department) -> Tutor:
     res.save()
     return res
 
-def test_all_tutors_dept(client_query,
-                    tutor_info : Tutor, 
-                    tutor_reseaux : Tutor):
-    query='''
-        query {
-            tutors (dept : \"RT\") {
-                edges {
-                    node {
-                        username
-                    }
-                }
-            }
-        }
-    '''
-    res = execute_query (client_query, query, "tutors")
-    data = get_data(res)
-    assert tutor_reseaux.username in data["username"]
-    assert tutor_info.username not in data["username"]
+""" Tests query
+"""
+# def test_all_tutors_dept(client_query,
+#                     tutor_info : Tutor, 
+#                     tutor_reseaux : Tutor):
+#     query='''
+#         query {
+#             tutors (dept : \"RT\") {
+#                 edges {
+#                     node {
+#                         username
+#                     }
+#                 }
+#             }
+#         }
+#     '''
+#     res = execute_query (client_query, query, "tutors")
+#     data = get_data(res)
+#     assert tutor_reseaux.username in data["username"]
+#     assert tutor_info.username not in data["username"]
 
-def test_tutors_algo(client_query,
-                           tutor_algo_prog: Tutor, scheduled1 : ScheduledCourse, scheduled2 : ScheduledCourse):
-    query='''
-        query {
-            tutors (dept : \"MIASHS\", week : 7, year : 2021){
-                edges {
-                    node {
-                        username
-                        firstName
-                    }
+# def test_tutors_algo(client_query,
+#                            tutor_algo_prog: Tutor, scheduled1 : ScheduledCourse, scheduled2 : ScheduledCourse):
+#     query='''
+#         query {
+#             tutors (dept : \"MIASHS\", week : 7, year : 2021){
+#                 edges {
+#                     node {
+#                         username
+#                         firstName
+#                     }
+#                 }
+#             }
+#         }
+#     '''
+#     res = execute_query (client_query, query, "tutors")
+#     data = get_data(res)
+#     assert tutor_algo_prog.first_name in data["firstName"]
+#     assert tutor_algo_prog.username in data["username"]
+    
+""" Tests mutations
+"""
+def test_mutations(db, client_query, department_miashs : Department, department_info : Department, department_reseaux : Department, capsys):
+    dpt_info_id = to_global_id('Department', department_info.id)
+    dpt_reseaux_id = to_global_id('Department', department_reseaux.id)
+    dpt_miashs_id = to_global_id('Department', department_miashs.id)
+    create = """
+        mutation {
+            createTutor (
+                username : "usr1"
+                firstName : "fst1"
+                lastName : "lst1"
+                email : "fts1.lst1@mail.com"
+                password : "azerty"
+                departments : [\"""" + dpt_info_id + "\", \"" + dpt_reseaux_id + \
+                """\"] ) {
+                tutor {
+                    id
                 }
             }
-        }
-    '''
-    res = execute_query (client_query, query, "tutors")
-    data = get_data(res)
-    assert tutor_algo_prog.first_name in data["firstName"]
-    assert tutor_algo_prog.username in data["username"]
-    
+            }
+    """
+
+    global_id = execute_mutation(client_query, create, "createTutor", "tutor")
+    try:
+        obj_id = from_global_id(global_id)[1]
+        obj = Tutor.objects.get(id=obj_id)
+        with capsys.disabled():
+            print("The object was created successfully")
+        update = """
+        mutation {
+            updateTutor ( 
+                id : \"""" + global_id + \
+                """\" username : "usr2"
+                isActive : false
+                departments : [\"""" + dpt_miashs_id + \
+                """\"] ) {
+                tutor {
+                    id
+                }
+            }
+            }
+        """
+
+        execute_mutation(client_query, update, "updateTutor", "tutor")
+        obj_updated = Tutor.objects.get(id=obj_id)
+        upd = (obj.username != obj_updated.username) and (obj.is_active != obj_updated.is_active) and (obj.departments != obj_updated.departments)
+        assert upd
+        if upd:
+            with capsys.disabled():
+                print("The object was updated successfully")
+        else:
+            with capsys.disabled():
+                print("The object was not updated")
+
+        delete = """
+        mutation {
+            deleteTutor ( 
+                id : \"""" + global_id + \
+                """\" ) {
+                tutor {
+                    id
+                }
+            }
+            }
+        """
+        execute_mutation(client_query, delete, "deleteTutor", "tutor")
+        try:
+            obj_deleted = Tutor.objects.get(id=obj_id)
+            with capsys.disabled():
+                print("The object was not deleted")
+        except Tutor.DoesNotExist:
+            with capsys.disabled():
+                print("The object was deleted successfully")
+
+
+    except Tutor.DoesNotExist:
+        with capsys.disabled():
+            print("The object was not created")
+        assert False
