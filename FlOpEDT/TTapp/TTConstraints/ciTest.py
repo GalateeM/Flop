76e5b32da563760009efa9b1dc2de940733d58fb
LauricTsr <lrct.teyre31@gmail.com>
2023-03-23 08:26:39 +0000

    Ci interpolation

---
+++ FlOpEDT/TTapp/TTConstraints/ciTest.py
@@ -0,0 +1,44 @@
+import os
+import json
+import re
+from pathlib import Path
+#lists of discarted files
+corrupted = []
+unavailable_pics = []
+#available languages
+languages = ["fr/","en/"]
+path = 'doc/'
+for language in languages:
+    entries = next(os.walk(path+language))[2]
+    for file_name in entries:
+        f = open(path+language+file_name,'r')
+        fString = f.read()
+        #Discard files with components tags
+        if((re.findall("<[ ]*component[ ]*",fString))):
+            corrupted.append(file_name)
+        #Discard files with unavailable pics's path
+        else:
+            invalid_path = False
+            founds = re.findall("(?:[!]\[(.*?)\])\((\.\.(.*?))\)",fString)
+            for found in founds:
+                if(not invalid_path):
+                    pic = found[2]
+                    pathPic = Path(path+pic)
+                    if(not pathPic.is_file()):
+                        invalid_path = True
+            if(invalid_path):
+                unavailable_pics.append(file_name)
+        
+        f.close()
+#Warning when a file is corrupted
+if(len(corrupted)>0):
+    print("\033[91mWARNING!! Check corrupted files: \033[00m")
+    for file in corrupted:
+        print(" - "+file)
+    print(" end")
+#Append lists of discarded files and write them in a json
+list_discarded = list(set().union(list(corrupted), list(unavailable_pics)))
+version_json = json.dumps(list_discarded)
+with open("../../discarded.json",'w') as file:
+    file.write('{"discarded": '+version_json+'}')
+file.close()
