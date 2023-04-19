fb92416b055cd776ee023714003e085b4759b4af
csl0779a <leo.cusseau@univ-tlse3.fr>
2023-04-19 14:51:53 +0200

    Quasar setup

--- FlOpEDT-front/src/main.ts
+++ FlOpEDT-front/src/main.ts
@@ -11,9 +11,18 @@ import '@vuepic/vue-datepicker/dist/main.css'
 import router from '@/router'
 import { FlopWeek } from '@/ts/type'
 import Popper from 'vue3-popper'
-
+    
 
 import i18n from '@/i18n'
+
+import { Quasar } from 'quasar'
+import Plugin from '@quasar/quasar-ui-qcalendar/src/QCalendarDay.js'
+import '@quasar/quasar-ui-qcalendar/src/css/calendar-day.sass'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
 // Provide the current week and year
@@ -29,5 +38,7 @@ const currentWeek: Ref<FlopWeek> = ref({
 
 app.provide('currentWeek', readonly(currentWeek.value))
 app.component('PopperComponent', Popper)
-app.use(router).use(createPinia()).use(i18n).mount('#app')
+app.use(router).use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+}).use(Plugin).use(createPinia()).use(i18n).mount('#app')
 
