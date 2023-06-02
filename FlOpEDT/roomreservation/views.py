d899d5a9a183e7c5c20507c39625b42e278f6807
GalateeM <galatee.m@gmail.com>
2023-06-02 11:57:57 +0200

    delete reservation

--- FlOpEDT/roomreservation/views.py
+++ FlOpEDT/roomreservation/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 
 from base.models import Room
 from .models import *
@@ -38,3 +39,14 @@ def RoomReservationRefuse(request, uuid, **kwargs):
     db_data = {'dept': request.department.abbrev, 'api': reverse('api:api_root'),
                'user_id': request.user.id, 'accept':False}
     return render(request, 'roomreservation/index.html', {'json_data': db_data})
+
+def RoomReservationRefuseConfirmed(request, uuid, **kwargs):
+    bad_response = {'status': 'KO', 'more': ''}
+    good_response = {'status': 'OK', 'more': ''}
+    try:
+        reservation_request = RoomReservation.objects.all().get(id_mail_validation=uuid)
+        reservation_request.delete()
+        return JsonResponse(good_response)
+    except:
+        return JsonResponse(bad_response)
+
